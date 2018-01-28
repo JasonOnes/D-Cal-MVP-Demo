8e84f0948b10cc69f3a0066e4c4900796e86b3aa
JasonOnes <jasonr.jones14@gmail.com>
2018-01-27 17:57:49 -0800

    initial simple cal forms

---
+++ main.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, redirect
+import calendar
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def pass_through():
+    # just redirects to first form so we don't have to type in url path from start
+    return redirect('/pick-mon')
+
+@app.route('/pick-mon', methods=['GET', 'POST'])
+def month_pick():
+    # renders the pick a month form
+    return render_template('pick-mon.html')
+   
+@app.route('/date-meal', methods=['POST'])
+def display_month_calendar():
+    # writes the html for the calendar of the month user picked for display above the day and meal form
+    mon = request.form['month']
+    html_cal = calendar.HTMLCalendar()
+    cal_x = html_cal.formatmonth(2018, int(mon))
+    return render_template('/date-meal.html', cal=cal_x, month=mon)
+
+@app.route('/meal-confirm/<month>', methods=['POST'])
+def confirmation(month):
+    # displays a confirmation of meal and date 
+    meal = request.form['meal']
+    date = request.form['date']
+    month_by_name = calendar.month_name[int(month)]
+    # TODO here is where we would create cal object for google calendar api 
+    return render_template('/meal-confirm.html', m=meal, d=date, month=month_by_name)
+
+
+if __name__ == "__main__":
+    app.run()
